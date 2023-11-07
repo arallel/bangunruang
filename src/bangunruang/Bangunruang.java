c6b186d1591702ac243c9fda584a637580718c33
Arallel <94737179+arallel@users.noreply.github.com>
2023-11-07 13:35:14 +0700

    fic

--- src/bangunruang/Bangunruang.java
+++ src/bangunruang/Bangunruang.java
@@ -37,8 +37,7 @@ public class Bangunruang {
                 System.out.println("==========================");
                 System.out.println("Input sisi kubus");
                 r = input.nextInt();
-                vol = r*r*r;
-                System.out.println("Volume kubus adalah: " + vol);
+                System.out.println("Volume kubus adalah: " + kubus(r));
                 break;
             case 2 :
                 System.out.println("Hitung Bangun Ruang balok:");
@@ -49,8 +48,7 @@ public class Bangunruang {
                 l = input.nextInt();
                 System.out.println("Input tinggi balok");
                 t = input.nextInt();
-                vol = p*l*t;
-                System.out.println("Volume kubus adalah: " + vol);
+                System.out.println("Volume balok adalah: " + balok(p,l,t));
                 break;
             case 3 :
                 System.out.println("Hitung Bangun Ruang tabung:");
@@ -59,18 +57,16 @@ public class Bangunruang {
                 r = input.nextInt();
                 System.out.println("Input tinggi tabung");
                 t = input.nextInt();
-                vol = phi*r*r+t;
-                System.out.println("Volume kubus adalah: " + vol);
+                
+                System.out.println("Volume tabung adalah: " + tabung(r,t,phi));
                 break;
             case 4 :
                 System.out.println("Hitung Bangun Ruang Bola:");
                 System.out.println("==========================");
-                System.out.println("Input Jari-jari tabung");
+                System.out.println("Input Jari-jari bola");
                 r = input.nextInt();
-                System.out.println("Input tinggi tabung");
-                t = input.nextInt();
-                vol = phi*r*r+t;
-                System.out.println("Volume kubus adalah: " + vol);
+                
+                System.out.println("Volume bola adalah: " + bola(r,phi));
                 break;
             case 5 :
                 System.out.println("Hitung Bangun Ruang volume kerucut:");
@@ -79,11 +75,30 @@ public class Bangunruang {
                 r = input.nextInt();
                 System.out.print("Input tinggi kerucut: ");
                 t = input.nextInt();
-                vol = (1.0 / 3.0) * phi * r * r * t;
+                
                 System.out.println();
-                System.out.println("Volume Kerucut = "+vol);
+                System.out.println("Volume Kerucut = "+kerucut(r,t,phi));
                 break;
         }
     }
-    
+   static double kubus(int r){
+       double vol = r*r*r;
+       return vol;
+   }
+   static double balok(int p,int l,int t){
+       double vol = p*l*t;
+       return vol;
+   }
+   static double tabung(int r,int t,double phi){
+       double vol = phi*r*r+t;
+       return vol;
+   }
+   static double bola(int r,double phi){
+       double vol = (4 * phi * r * r * r) / 3;
+       return vol;
+   }
+   static double kerucut(int r,int t,double phi){
+       double vol = (1 * phi * r * r * t) / 3;
+       return vol;
+   }
 }
